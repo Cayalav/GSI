1459bab246cb04d7b75d7c952413fc30c3ed0dfd
ayalaac <ayalaac@SU5-03.uninorteacad.uninorte.edu.co>
2018-01-26 11:09:42 -0500

    Random num

---
+++ src/gsi/GSI.java
@@ -0,0 +1,35 @@
+package gsi;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GSI {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digita los numeros a generar");
+        int num = sc.nextInt();
+        System.out.println("Cuantos digitos quiere que contenga?");
+        int dig = sc.nextInt();
+        
+        int dig1 = 10*dig;
+        long[] vector = new long[num];
+
+        Random randomGenerator = new Random();
+        for (int idx = 1; idx <= num; ++idx) {
+            //3 digitos
+            //    (9)+1
+            //int dig3 = rng.nextInt(900)+100; //siempre 3 digitos
+            //int dig5 = rng.nextInt(90000)+10000; //siempre 5 digitos
+            //long dig13 = rng.nextLong(9000000000000L)+1000000000000L; //siempre 13 digitos
+            long randomInt = randomGenerator.nextInt(900)+100;
+            System.out.println(randomInt);
+            
+//            vector[idx] = randomInt;
+            
+        }
+
+    }
+
+
+}
